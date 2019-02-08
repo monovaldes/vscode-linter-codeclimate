09a6b6e5eab65425fcb1e4cb23ae4847b3b2bb0e
Ivan Valdes Riesco <monovaldes@gmail.com>
2019-02-08 16:54:54 -0300

    added typescirpt support, workspaceless-file scan

--- src/codeclimate.ts
+++ src/codeclimate.ts
@@ -35,63 +35,55 @@ export default class CodeClimateLintingProvider{
     vscode.workspace.textDocuments.forEach(this.doCClint, this);
   }
 
+  private lintCCStreamItem(item, textDocument, diagnostics) {
+    try {
+      let severity = item.severity.toLowerCase() === "minor" ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Error;
+      let message = `[codeclimate/${item.engine_name} - ${item.categories.toString()}] ${item.description}`;
+      if (item.check_name === "similar-code") {
+        message += '\nFiles:\n';
+        item.other_locations.forEach( it => {
+          message += ` ${it.path} [${it.lines.begin}-${it.lines.end}]\n`
+        })
+      }
+      const loc = item.location
+      let range = null;
+      if ('positions' in loc) {
+        range = new vscode.Range(item.location.positions.begin.line - 1, item.location.positions.begin.column -1, item.location.positions.begin.line - 1, 80);
+      } else if ('lines' in loc) {
+        range = new vscode.Range(item.location.lines.begin - 1, 0, item.location.lines.begin - 1, 80);
+      }
+      let diagnostic = new vscode.Diagnostic(range, message, severity);
+      diagnostics.push(diagnostic);
+    } catch (error) {
+      console.log(error);
+      this.completeTask(textDocument.fileName);
+    }
+  }
+
   private doCClint() {
     const conf = vscode.workspace.getConfiguration();
-    if (!('linter_codeclimate' in conf && conf.linter_codeclimate.enabled) || vscode.window.activeTextEditor === null) { return; }
+    if (!('linter_codeclimate' in conf && conf.linter_codeclimate.enabled) || vscode.window.activeTextEditor === null ) { return; }
     const textDocument = vscode.window.activeTextEditor.document;
 
     if(this.activeDocuments.indexOf(textDocument.fileName) !== -1 ) return;
     this.activeDocuments.push(textDocument.fileName);
 
     let diagnostics: vscode.Diagnostic[] = [];
-    const exec_path = path.relative(vscode.workspace.rootPath, textDocument.fileName);
+    const cwd = vscode.workspace.rootPath || path.dirname(textDocument.fileName);
+    const exec_path = path.relative(cwd, textDocument.fileName);
     let resultStream = ''
     try {
-      const ls = cp.spawn('codeclimate', ['analyze', '-f', 'json', exec_path], {
-        cwd: vscode.workspace.rootPath,
-        detached: true,
-        stdio: [ 'ignore', 'pipe', 'pipe' ]
-      });
+      const ls = cp.spawn('codeclimate', ['analyze', '-f', 'json', exec_path], { cwd: cwd, detached: true, stdio: [ 'ignore', 'pipe', 'pipe' ] });
 
-      ls.stdout.on('data', (data) => {
-        resultStream += data;
-      });
+      ls.stdout.on('data', (data) => { resultStream += data; });
       
-      ls.stderr.on('data', (data) => {
-        console.log(`Codeclimate Extension Execution Failed:\n${data}`)
-      });
+      ls.stderr.on('data', (data) => { console.log(`Codeclimate Extension Execution Failed:\n${data}`) });
       
       ls.on('close', () => {
         if (resultStream !== '') {
-          JSON.parse(resultStream).forEach( item => {
-            try {
-              if ('severity' in item) {
-                let severity = item.severity.toLowerCase() === "minor" ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Error;
-                let message = `[codeclimate/${item.engine_name} - ${item.categories.toString()}] ${item.description}`;
-                if (item.check_name === "similar-code") {
-                  message += '\nFiles:\n';
-                  item.other_locations.forEach( it => {
-                    message += ` ${it.path} [${it.lines.begin}-${it.lines.end}]\n`
-                  })
-                }
-                const loc = item.location
-                let range = null;
-                if ('positions' in loc) {
-                  range = new vscode.Range(item.location.positions.begin.line - 1, item.location.positions.begin.column -1, item.location.positions.begin.line - 1, 80);
-                } else if ('lines' in loc) {
-                  range = new vscode.Range(item.location.lines.begin - 1, 0, item.location.lines.begin - 1, 80);
-                }
-                let diagnostic = new vscode.Diagnostic(range, message, severity);
-                diagnostics.push(diagnostic);
-              }
-            } catch (error) {
-              console.log(error);
-              this.completeTask(textDocument.fileName);
-            }
-          });
+          JSON.parse(resultStream).forEach( item => { if ('severity' in item) { this.lintCCStreamItem(item, textDocument, diagnostics) } });
           this.diagnosticCollection.set(textDocument.uri, diagnostics);
         }
-
         this.completeTask(textDocument.fileName);
       });
     } catch (error) {
